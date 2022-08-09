d9e2c26da0797b7288e3ae1f47a96be2a1627b61
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-08-08 23:50:15 -0300

    Project started

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { isItFriday } = require('../../lib');
+
+Given('today is {string}', function (givenDay) {
+    this.today = givenDay;
+});
+
+When('I ask whether it\'s Friday yet', function () {
+    this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+    assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
